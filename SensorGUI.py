f3128c07d6bdd9bd48113b6d031910ac4d242feb
Dennis Ripp <dennis_ripp@outlook.de>
2022-12-11 11:56:09 +0100

    refactor

--- SensorGUI.py
+++ SensorGUI.py
@@ -1,36 +1,30 @@
 import PySimpleGUI as sg
+import PySimpleGUI.PySimpleGUI
 from gmqtt import Client as MQTTClient
 import easy_mqtt as easy
 import asyncio
 import json
 
 
-window_dict = {
-    "eg_1" : False,
-    "eg_2" : False,
-    "og_1" : False,
-    "og_2" : False,
-}
-
-thermo_dict = {
-    "eg_1": "20",
-    "og_1": "20",
-}
-
-eg_thermostat = 0
-eg_jalousie = False
-eg_licht = False
-
-og_thermostat = 0
-og_jalousie = False
-og_licht = False
-
-
 ec: easy.EasyMqtt
+win: PySimpleGUI.PySimpleGUI.Window
+
+device_dict = {
+    "eg_1_window" : False,
+    "eg_2_window" : False,
+    "og_1_window" : False,
+    "og_2_window" : False,
+    "eg_1_thermo": "20",
+    "og_1_thermo": "20",
+    "eg_1_jalousie": False,
+    "og_1_jalousie": False,
+    "eg_1_light": False,
+    "og_1_light": False,
+}
 
 
 def on_message(client: MQTTClient, topic, payload, qos, properties):
-    global window_dict, thermo_dict
+    global device_dict, win
 
     message = payload.decode()
     msg_json = json.loads(message)
@@ -39,10 +33,17 @@ def on_message(client: MQTTClient, topic, payload, qos, properties):
     msg_value = msg_json["value"]
     msg_device = msg_json["device"]
 
-    if "window" in msg_device:
-        window_dict[f"{msg_loc}_{msg_id}"] = msg_value
-    if "thermo" in msg_device:
-        thermo_dict[f"{msg_loc}_{msg_id}"] = msg_value
+    key = f"{msg_loc}_{msg_id}_{msg_device}"
+    device_dict[key] = msg_value
+
+    if "jalousie" in key:
+        win[key].Update(text=returnKontaktJalousi(msg_value))
+    elif "light" in key:
+        win[key].Update(text=get_licht(msg_value))
+    elif "window" in key:
+        win[key].Update(returnKontaktJalousi(msg_value))
+    else:
+        win[key].Update(msg_value)
 
     print(f"got message: {message}")
 
@@ -65,14 +66,14 @@ def returnKontaktJalousi(bool):
         return "Zu"
 
 
-def getLicht(bool):
-    if (bool):
+def get_licht(val):
+    if val:
         return "An"
     else:
         return "Aus"
 
-    
-def createMQTTJason(loc, device, device_id, value):
+
+def create_json(loc, device, device_id, value):
     data = {
         "loc": loc,
         "device": device,
@@ -84,18 +85,18 @@ def createMQTTJason(loc, device, device_id, value):
 
 layout = [
     [sg.Text('Erdgeschoss')],
-    [sg.Text('Thermostat', key='eg_thermostat_1_text'), sg.InputText("20", key="eg_thermostat_1", size=(10, 2)), sg.Button("OK", key="eg_thermo_1_button")],
-    [sg.Text('Fenster 1', key='eg_fenster_1_text'), sg.Text(returnKontaktJalousi(window_dict["eg_1"]), key='eg_window_1_state') , sg.Button("get status", key="eg_fenster_1_button")],
-    [sg.Text('Fenster 2', key='eg_fenster_2_text'),sg.Text(returnKontaktJalousi(window_dict["eg_2"]), key='eg_window_2_state') , sg.Button("get status", key="eg_fenster_2_button")],
-    [sg.Text('Jalousie', key='eg_jalousi_1_text'), sg.Button(returnKontaktJalousi(eg_jalousie), key="eg_jalousie_1_button")],
-    [sg.Text('Licht', key='eg_licht_1_text'), sg.Button(getLicht(eg_licht), key="eg_licht_1_button")],
+    [sg.Text('Thermostat'), sg.InputText("20", key="eg_1_thermo", size=(10, 2)), sg.Button("OK", key="eg_thermo_1_button")],
+    [sg.Text('Fenster 1'), sg.Text(returnKontaktJalousi(device_dict["eg_1_window"]), key='eg_1_window') , sg.Button("get status", key="eg_fenster_1_button")],
+    [sg.Text('Fenster 2'),sg.Text(returnKontaktJalousi(device_dict["eg_2_window"]), key='eg_2_window') , sg.Button("get status", key="eg_fenster_2_button")],
+    [sg.Text('Jalousie'), sg.Button(returnKontaktJalousi(device_dict["eg_1_jalousie"],), key="eg_1_jalousie")],
+    [sg.Text('Licht'), sg.Button(get_licht(device_dict["eg_1_light"], ), key="eg_1_light")],
     [],
     [sg.Text('Obergeschoss')],
-    [sg.Text('Thermostat', key='og_thermostat_1_text'), sg.InputText("20", key="og_thermostat_1", size=(10, 2)), sg.Button("OK", key="og_thermo_1_button")],
-    [sg.Text('Fenster 1', key='og_fenster_1_text'), sg.Text(returnKontaktJalousi(window_dict["og_1"]), key='og_window_1_state') , sg.Button("get status",  key="og_fenster_1_button")],
-    [sg.Text('Fenster 2', key='og_fenster_2_text'), sg.Text(returnKontaktJalousi(window_dict["eg_2"]), key='og_window_2_state') , sg.Button("get status",  key="og_fenster_2_button")],
-    [sg.Text('Jalousie', key='og_jalousi_1_text'), sg.Button(returnKontaktJalousi(og_jalousie), key="og_jalousie_1_button")],
-    [sg.Text('Licht', key='og_licht_1_text'), sg.Button(getLicht(og_licht), key="og_licht_1_button")],
+    [sg.Text('Thermostat'), sg.InputText("20", key="og_1_thermo", size=(10, 2)), sg.Button("OK", key="og_thermo_1_button")],
+    [sg.Text('Fenster 1'), sg.Text(returnKontaktJalousi(device_dict["og_1_window"]), key='og_1_window') , sg.Button("get status",  key="og_fenster_1_button")],
+    [sg.Text('Fenster 2'), sg.Text(returnKontaktJalousi(device_dict["og_2_window"]), key='og_2_window') , sg.Button("get status",  key="og_fenster_2_button")],
+    [sg.Text('Jalousie'), sg.Button(returnKontaktJalousi(device_dict["og_1_jalousie"],), key="og_1_jalousie")],
+    [sg.Text('Licht'), sg.Button(get_licht(device_dict["og_1_light"]), key="og_1_light")],
     [],
     [],
     [sg.Button("verlasse das haus", key="routine_leave_home")],
@@ -103,31 +104,28 @@ layout = [
 ]
 
 
-def updateButtonText(window, windowKey , text):
-    window[windowKey].Update(text=text)
-
-
-def send_message(floor, device, id, value):
+def send_message(floor, device, device_id, value):
     global ec
-    ec.publish(f"home/{floor}/{device}/{id}", createMQTTJason(f"{floor}", f"{device}", id, value), qos=1)
+    ec.publish(f"home/{floor}/{device}/{device_id}", create_json(f"{floor}", f"{device}", device_id, value), qos=1)
 
 
 async def warn_open_window(window):
-    global window_dict
+    global device_dict, win
     send_message("eg", "window", 1, True)
     send_message("eg", "window", 2, True)
     send_message("og", "window", 1, True)
     send_message("og", "window", 2, True)
 
     await asyncio.sleep(0.5)
-    anyopen = False
+    any_open = False
     open_list = []
-    for cur_win in window_dict.keys():
-        is_open = window_dict[cur_win]
-        if is_open:
-            anyopen = True
-            open_list.append(f"window {cur_win}")
-    if anyopen:
+    for cur_win in device_dict.keys():
+        if "window" in cur_win:
+            is_open = device_dict[cur_win]
+            if is_open:
+                any_open = True
+                open_list.append(f"{cur_win}")
+    if any_open:
         window.Popup(f"{open_list} still open!! ")
 
 
@@ -143,92 +141,69 @@ async def leave_house_routine(window):
 
     await warn_open_window(window)
 
+
 async def good_morning_routine(window, sg):
-    global thermo_dict
+    global device_dict
     send_message("og", "light", 1, True)
     send_message("og", "jalousie", 1, True)
     send_message("og", "thermo", 1, "22")
 
     await asyncio.sleep(0.5)
 
-    updated_temperature = thermo_dict["og_1"]
-    sg["og_thermostat_1"].Update(updated_temperature)
+    updated_temperature = device_dict["og_1_thermo"]
+    sg["og_1_thermo"].Update(updated_temperature)
     window.Popup(f"Good morning!! temperature was set to {updated_temperature}")
 
+
 async def main_window():
-    global eg_licht, eg_jalousie, eg_thermostat
-    global og_licht, og_jalousie, og_thermostat
-    global ec, window_dict
+    global ec, win
 
     sg.theme("Dark")
     window = sg.Window('Overview', layout, size=(400, 500))
-
+    win = window
     while True:
         window.refresh()
         event, values = window.read(timeout = 0)
         if event == sg.WINDOW_CLOSED:
             break
 
-        ## Routine
+        # Routine ---------------------------------
         if event == "routine_leave_home":
             await leave_house_routine(sg)
 
         if event == "routine_good morning":
             await good_morning_routine(sg, window)
 
-        ## EG
+        # EG ---------------------------------
         if event == "eg_thermo_1_button":
-            send_message("eg", "thermo", 1,  values["eg_thermostat_1"])
+            send_message("eg", "thermo", 1,  values["eg_1_thermo"])
 
         if event == "eg_fenster_1_button":
             send_message("eg", "window", 1, True)
-            await asyncio.sleep(0.5)
-            window["eg_window_1_state"].Update(returnKontaktJalousi(window_dict["eg_1"]))
 
         if event == "eg_fenster_2_button":
-            send_message("eg", "window", 2, True)
-            await asyncio.sleep(0.5)
-            window["eg_window_2_state"].Update(returnKontaktJalousi(window_dict["eg_2"]))
+            send_message("eg", "window", 2,True)
 
-        if event == "eg_jalousie_1_button":
-            eg_jalousie = not eg_jalousie
-            updateButtonText(window, event, returnKontaktJalousi(eg_jalousie))
-            send_message("eg", "jalousie", 1, eg_jalousie)
+        if event == "eg_1_jalousie":
+            send_message("eg", "jalousie", 1, not device_dict[event])
 
-        if event == "eg_licht_1_button":
-            eg_licht = not eg_licht
-            updateButtonText(window, event, getLicht(eg_licht))
-            send_message("eg", "light", 1, eg_licht)
-        ## OG
+        if event == "eg_1_light":
+            send_message("eg", "light", 1, not device_dict[event])
+        # OG ---------------------------------
         if event == "og_thermo_1_button":
-            send_message("og", "thermo", 1,  values["og_thermostat_1"])
+            send_message("og", "thermo", 1, values["og_1_thermo"])
 
         if event == "og_fenster_1_button":
             send_message("og", "window", 1, True)
-            await asyncio.sleep(0.5)
-            window["og_window_1_state"].Update(returnKontaktJalousi(window_dict["og_1"]))
 
         if event == "og_fenster_2_button":
             send_message("og", "window", 2, True)
-            await asyncio.sleep(0.5)
-            window["og_window_2_state"].Update(returnKontaktJalousi(window_dict["og_2"]))
 
-        if event == "og_jalousie_1_button":
-            og_jalousie = not og_jalousie
-            updateButtonText(window, event, returnKontaktJalousi(og_jalousie))
-            send_message("og", "jalousie", 2, og_jalousie)
+        if event == "og_1_jalousie":
+            send_message("og", "jalousie", 1, not device_dict[event])
 
-        if event == "og_licht_1_button":
-            og_licht = not og_licht
-            updateButtonText(window, event, getLicht(og_licht))
-            send_message("og", "light", 1, og_licht)
+        if event == "og_1_light":
+            send_message("og", "light", 1, not device_dict[event])
         await asyncio.sleep(0.1)
 
     window.close()
-
-
-async def main():
-    await asyncio.gather(init_client(), main_window())
-
-if __name__ == "__main__":
-    asyncio.run((main()))
--- main.py
+++ main.py
@@ -7,39 +7,33 @@ import asyncio
 
 
 async def main():
+    instance_list = [
+        # windows
+        window_endpoint.MqttWindow("og", 1, "localhost", "window").init_client(),
+        window_endpoint.MqttWindow("og", 2, "localhost", "window").init_client(),
+        window_endpoint.MqttWindow("eg", 1, "localhost", "window").init_client(),
+        window_endpoint.MqttWindow("eg", 2, "localhost", "window").init_client(),
+        # lights
+        light_endpoint.MqttLight("eg", 1, "localhost", "light").init_client(),
+        light_endpoint.MqttLight("og", 1, "localhost", "light").init_client(),
+        # jalousie
+        jalousie_endpoint.MqttJalousie("eg", 1, "localhost", "jalousie").init_client(),
+        jalousie_endpoint.MqttJalousie("og", 1, "localhost", "jalousie").init_client(),
+        # thermostat
+        thermo_endpoint.MqttThermo("eg", 1, "localhost", "thermo").init_client(),
+        thermo_endpoint.MqttThermo("og", 1, "localhost", "thermo").init_client(),
+        # gui
+        SensorGUI.init_client(),
+        SensorGUI.main_window()
+    ]
 
-    og_fenster_1 = window_endpoint.MqttWindow("og", 1, "localhost", "window")
-    og_fenster_2 = window_endpoint.MqttWindow("og", 2, "localhost", "window")
-    eg_fenster_1 = window_endpoint.MqttWindow("eg", 1, "localhost", "window")
-    eg_fenster_2 = window_endpoint.MqttWindow("eg", 2, "localhost", "window")
-
-    eg_light_1 = light_endpoint.MqttLight("eg", 1, "localhost", "light")
-    og_light_1 = light_endpoint.MqttLight("og", 1, "localhost", "light")
-
-    eg_jalousie_1 = jalousie_endpoint.MqttJalousie("eg", 1, "localhost", "jalousie")
-    og_jalousie_1 = jalousie_endpoint.MqttJalousie("og", 1, "localhost", "jalousie")
-
-    eg_thermo_1 = thermo_endpoint.MqttThermo("eg", 1, "localhost", "thermo")
-    og_thermo_1 = thermo_endpoint.MqttThermo("og", 1, "localhost", "thermo")
-
-    asyncio.gather(
-    og_fenster_1.init_client(),
-    og_fenster_2.init_client(),
-    eg_fenster_1.init_client(),
-    eg_fenster_2.init_client(),
-    eg_light_1.init_client(),
-    og_light_1.init_client(),
-    eg_jalousie_1.init_client(),
-    og_jalousie_1.init_client(),
-    eg_thermo_1.init_client(),
-    og_thermo_1.init_client(),
-    SensorGUI.init_client(),
-    SensorGUI.main_window()
+    await asyncio.gather(
+        *instance_list
     )
 
     while True:
 
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.1)
 
 
 if __name__ == "__main__":
